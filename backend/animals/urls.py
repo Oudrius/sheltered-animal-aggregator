ebd1b848b2aa62139915a91de9dec59f9ed3bbba
Audrius <132777704+Oudrius@users.noreply.github.com>
2024-09-24 17:35:43 +0300

    Create views to read, update and delete single model instances

--- backend/animals/urls.py
+++ backend/animals/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path("city/", views.CityListCreate.as_view(), name='city-view-create'),
-    path("shelter/", views.ShelterListCreate.as_view(), name='shelter-view-create'),
+    path("cities/", views.CityListCreate.as_view(), name='city-view-create'),
+    path("city/<int:pk>/", views.CityRetrieveUpdateDestroy.as_view(), name='city-retrieve-update-destroy'),
+    path("shelters/", views.ShelterListCreate.as_view(), name='shelter-view-create'),
+    path("shelter/<int:pk>/", views.ShelterRetrieveUpdateDestroy.as_view(), name='shelter-retrieve-update-destroy'),
     path("species/", views.SpeciesListCreate.as_view(), name='species-view-create'),
-    path("animal/", views.AnimalListCreate.as_view(), name='animal-view-create')
+    path("species/<int:pk>/", views.SpeciesRetrieveUpdateDestroy.as_view(), name='species-retrieve-update-destroy'),
+    path("animals/", views.AnimalListCreate.as_view(), name='animal-view-create'),
+    path("animal/<int:pk>/", views.AnimalRetrieveUpdateDestroy.as_view(), name='animal-retrieve-update-destroy')
 ]
--- backend/animals/views.py
+++ backend/animals/views.py
@@ -7,6 +7,12 @@ from .serializers import CitySerializer, ShelterSerializer, SpeciesSerializer, A
 class CityListCreate(generics.ListCreateAPIView):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+    
+
+class CityRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    lookup_field = "pk"
 
 
 class ShelterListCreate(generics.ListCreateAPIView):
@@ -14,11 +20,29 @@ class ShelterListCreate(generics.ListCreateAPIView):
     serializer_class = ShelterSerializer
 
 
+class ShelterRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Shelter.objects.all()
+    serializer_class = ShelterSerializer
+    lookup_field = "pk"
+
+
 class SpeciesListCreate(generics.ListCreateAPIView):
     queryset = Species.objects.all()
     serializer_class = SpeciesSerializer
 
 
+class SpeciesRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Species.objects.all()
+    serializer_class = SpeciesSerializer
+    lookup_field = "pk"
+
+
 class AnimalListCreate(generics.ListCreateAPIView):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+
+
+class AnimalRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
+    lookup_field = "pk"
